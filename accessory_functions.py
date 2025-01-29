fix transform, normalization, train loop

accessory_functions.py
@@ -90,7 +90,7 @@ def balance_labels(labels_df):
     return pd.DataFrame(labels)
 
 # Takes a path to a 2D dicom file and converts it to a tensor (with dimensions interp_resolution x interp_resolution)
-def dicom_path_to_tensor(img_path,target_dim):
+def dicom_path_to_tensor(img_path,target_dim,train=True):
     # Load image dicom and pre-process into tensor for input into model
     dicom = dcmread(img_path)
     array = dicom.pixel_array
@@ -98,15 +98,23 @@ def dicom_path_to_tensor(img_path,target_dim):
     # array = rescale(array, dicom.PixelSpacing,anti_aliasing=dicom.PixelSpacing[0] < 1)
     #center crop to target_dim x target_dim, with 0 padding if less
     array = array.astype(np.uint8)
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.1181772], std=[0.13326852023601532]),
-        transforms.Pad((int(np.ceil(max(target_dim-array.shape[0],0)/2)),int(np.ceil(max(target_dim-array.shape[1],0)/2)))), #can add rotation/flipping here
-        transforms.CenterCrop((target_dim,target_dim)),
-        transforms.RandomRotation(degrees=(0,180)), #transforms copied from https://www.sciencedirect.com/science/article/pii/S1097664723003320?via%3Dihub, but instead of random transforms they did all permutations on each image
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomVerticalFlip(p=0.5)
-    ])
+    if train:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.1181772], std=[0.13326852023601532]),
+            transforms.Pad((int(np.ceil(max(target_dim-array.shape[0],0)/2)),int(np.ceil(max(target_dim-array.shape[1],0)/2)))), #can add rotation/flipping here
+            transforms.CenterCrop((target_dim,target_dim)),
+            transforms.RandomRotation(degrees=(0,180)), #transforms copied from https://www.sciencedirect.com/science/article/pii/S1097664723003320?via%3Dihub, but instead of random transforms they did all permutations on each image
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomVerticalFlip(p=0.5)
+        ])
+    else: # remove data augmentation when testing
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.1181772], std=[0.13326852023601532]),
+            transforms.Pad((int(np.ceil(max(target_dim-array.shape[0],0)/2)),int(np.ceil(max(target_dim-array.shape[1],0)/2)))), #can add rotation/flipping here
+            transforms.CenterCrop((target_dim,target_dim)),
+        ])
     array = transform(array)
     return array
 
@@ -121,7 +129,7 @@ class CustomImageDataset(Dataset):
     # all the examples in the given folder for training should have the same label
     # paths should contain the path to all the relevant PID folders
     # set balance to True if you want to use oversampling balance the positive and negative labels
-    def __init__(self, pids, labels, target_dim=224, balance=False):
+    def __init__(self, pids, labels, train=True, target_dim=224, balance=False):
         # if balance:
         #     self.img_labels = balance_labels(pd.read_csv(annotations_file, header=None)) # this won't work anymore
         self.all_paths = []
@@ -142,6 +150,7 @@ class CustomImageDataset(Dataset):
         all_paths,all_labels = zip(*paired_paths_labels)
         self.all_paths = list(all_paths)
         self.all_labels = list(all_labels)
+        self.train = train #store whether this is a training set or a validation/test set
         self.target_dimensions = target_dim # Note we should keep this at 224x224 since that is what ResNet is built for/trained on
 
     def __len__(self):
@@ -150,9 +159,8 @@ class CustomImageDataset(Dataset):
     # Loads the dicoms and label of a given MRI, converts DICOMS into a 3Dntensor,
     # interpolates to a given size, and returns image tensor and label
     def __getitem__(self, idx):
-        #img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
         img_path = self.all_paths[idx]
-        image = dicom_path_to_tensor(img_path,self.target_dimensions)
+        image = dicom_path_to_tensor(img_path,self.target_dimensions,self.train)
         return image, torch.tensor([self.all_labels[idx]],dtype=torch.float32)
 
 
@@ -163,10 +171,7 @@ def train_loop(dataloader, model, loss_fn, device, optimizer):
     current_loss = 0
     all_pred_proba = []
     all_y = []
-    for batch in range(len(dataloader)):
-
-        (X, y) = next(iter(dataloader))
-
+    for batch, (X,y) in enumerate(dataloader):
         # Move X and y to GPU
         X = X.to(device)
         y = y.to(device)
@@ -246,28 +251,4 @@ def test_loop(dataloader, model, loss_fn, device):
     print(f"Test Error: \n   Accuracy: {accuracy:>0.3f}\n   recall: {recall:>0.3f}\n   specificity: {specificity:>0.3f}\n   precision: {precision:>0.3f}\n   AUC: {auc:>0.3f}\n   Avg loss: {test_loss:>8f} \n")
     wandb.log({'test_loss': test_loss,"test_acc":accuracy,"test_precision":precision,"test_recall":recall,"test_specificity":specificity,"test_auc":auc})
     return all_pred, all_y
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
-       
        