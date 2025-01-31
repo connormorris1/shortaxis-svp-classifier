31b710242f52a40c76ad5a7c22ca9c7efbc65581
Connor Morris <con.morris09@gmail.com>
2025-01-31 13:40:53 -0800

    update analysis csv script for new file structure

--- gen_analysis_csv.py
+++ gen_analysis_csv.py
@@ -3,6 +3,8 @@ from pydicom import dcmread
 import models
 import torch
 from accessory_functions import dicom_path_to_tensor
+import random
+import os
 import argparse
 
 #read in test dataset
@@ -11,48 +13,72 @@ import argparse
 #save score dfs
 
 parser = argparse.ArgumentParser(description='Generate CSVs to further analyze data')
-parser.add_argument('--data_path', type=str, required=True, help='path to test set csv')
-parser.add_argument('--model', type=str,choices=['resnet18','resnet34','resnet50'],required=True,help='Which model architecture to use')
+parser.add_argument('--base_path', type=str, required=True, help='path to parent directory of dataset')
+parser.add_argument('--model', type=str,choices=['vgg11','resnet18','resnet34','resnet50'],required=True,help='Which model architecture to use')
 parser.add_argument('--model_weights', type=str,required=True,help='Path to trained model weights')
 parser.add_argument('--save_path', type=str,help='path to save dataset to',required=True)
 
 args = parser.parse_args()
 
-dataset_path = args.data_path
-df = pd.read_csv(dataset_path,header=None)
+base_path = args.base_path
+print(f"Generating analysis CSV for {base_path} with model {args.model} and weights {args.model_weights}.")
 if args.model == 'resnet18':
     model = models.resnet18()
 elif args.model == 'resnet34':
     model = models.resnet34()
 elif args.model == 'resnet50':
     model = models.resnet50()
+elif args.model == 'vgg11':
+    model = models.vgg11()
 model.load_state_dict(torch.load(args.model_weights))
 model.eval()
+
+#extract test set PID paths
+normal_path = os.path.join(base_path,'normal')
+normal_pids = [] #will be list of full paths to all pid directories
+for pid in os.listdir(normal_path):
+    normal_pids.append(os.path.join(normal_path,pid))
+
+svp_path = os.path.join(base_path,'svp')
+svp_pids = [] #will be list of full paths to all pid directories
+for pid in os.listdir(svp_path):
+    svp_pids.append(os.path.join(svp_path,pid))
+
+random.seed(10)
+test_svp_pids = random.sample(svp_pids,6) #each PID should be a full path to that PID directory
+test_normal_pids = random.sample(normal_pids,9)
+
 all_z_axis = []
 all_time = []
 all_pid = []
 all_scores = []
-print(f'Length of dataset: {len(df)}')
+df = pd.DataFrame(columns=['path','pid','date','series_description','z_coord','time','pred','label'])
 
-for i,path in enumerate(df[0]):
-    dcm = dcmread(path)
-    pid = dcm.PatientID
-    time = dcm.TriggerTime
-    loc = dcm.get((0x0020,0x1041),'Unknown').value
-    img = dicom_path_to_tensor(path).unsqueeze(0)
-    score = model(img)
-    score_sigmoid = torch.sigmoid(score)
-    score_sigmoid = score_sigmoid[0,0].item()
-    all_z_axis.append(loc)
-    all_time.append(time)
-    all_pid.append(pid)
-    all_scores.append(score_sigmoid)
-    if i % 1000 == 0:
-        print(i)
-
-df['z_coord'] = all_z_axis
-df['pid'] = all_pid
-df['time'] = all_time
-df['pred'] = all_scores
-df.rename(columns={0:'dcm_path',1:'label'},inplace=True)
+def gen_analysis_df(pid_paths,label,df):
+    #loop through all test images, extract metadata, test on model
+    for pid_path in pid_paths:
+        date_paths = os.listdir(pid_path)
+        for date_path in date_paths:
+            pid_date_path = os.path.join(pid_path,date_path)
+            for sequence in os.listdir(pid_date_path):
+                seq_path = os.path.join(pid_date_path,sequence)
+                for dcm in os.listdir(seq_path):
+                    dcm_path = os.path.join(seq_path,dcm)
+                    dcm = dcmread(dcm_path)
+                    pid = dcm.PatientID
+                    time = float(dcm.TriggerTime)
+                    loc = float(dcm.get((0x0020,0x1041),'Unknown').value)
+                    img = dicom_path_to_tensor(dcm_path,224,train=False).unsqueeze(0)
+                    date = dcm.StudyDate
+                    series_desc = dcm.SeriesDescription
+                    score = model(img)
+                    score_sigmoid = torch.sigmoid(score)
+                    score_sigmoid = score_sigmoid[0,0].item()
+                    df.loc[len(df)] = [dcm_path,pid,date,series_desc,loc,time,score_sigmoid,label] 
+    return df
+print("Generating analysis dataframe for SVPs")
+df = gen_analysis_df(test_svp_pids,1,df)
+print("Generating analysis dataframe for normals")
+df = gen_analysis_df(test_normal_pids,0,df)
+print(f"Saving analysis dataframe to {args.save_path}")
 df.to_csv(args.save_path,index=False)
